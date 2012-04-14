753cca948afe35c701d80fc37b3ffa48bd70a8d1
Mike <mikezter@gmail.com>
2012-04-14 15:43:46 +0200

    add watchr config

---
+++ spec/watchr.rb
@@ -0,0 +1,37 @@
+# Run me with:
+#   $ watchr spec/watchr.rb
+
+# --------------------------------------------------
+# Rules
+# --------------------------------------------------
+watch( '^spec/(.*)/(.*)_spec\.rb' ) { |m| rspec m[0] }
+watch( '^spec/(.*)_spec\.rb' ) { |m| rspec m[0] }
+watch( '^lib/(.*)\.rb' ) { |m| rspec "spec/unit/#{m[1]}_spec.rb" }
+watch( '^lib/(.*)/(.*)\.rb' ) { |m| rspec "spec/unit/#{m[1]}/#{m[2]}_spec.rb" }
+
+# --------------------------------------------------
+# Signal Handling
+# --------------------------------------------------
+Signal.trap('INT' ) { abort("\n") } # Ctrl-C
+
+# --------------------------------------------------
+# Helpers
+# --------------------------------------------------
+
+def rspec(path)
+  run "clear"
+  if not File.exists?(path)
+    puts "File #{path} does not exist!"
+  elsif File.read(path).include?(":focus => true")
+    run "bundle exec rspec -t @focus #{path}"
+  else
+    run "bundle exec rspec #{path}"
+  end
+end
+
+def run(cmd)
+  puts   cmd
+  system cmd
+end
+
+
