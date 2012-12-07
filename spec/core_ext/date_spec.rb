82f737ea2c0021a51a8f0e0e444e9073b8cb37c7
rschaden <rschaden@gmail.com>
2012-12-07 16:59:45 +0100

    adjust time critical tolerance

--- spec/core_ext/date_spec.rb
+++ spec/core_ext/date_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Date do
   it '#to_gm_time' do
     date = DateTime.now
-    date.to_gm_time.should be_within(1).of Time.now.utc
+    date.to_gm_time.should be_within(10).of Time.now.utc
   end
 
   it '#to_local_time' do
     date = DateTime.now.new_offset
-    date.to_local_time.should be_within(1).of Time.now
+    date.to_local_time.should be_within(10).of Time.now
   end
 
 end
