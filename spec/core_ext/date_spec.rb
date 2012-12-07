6e5a136c93418bd335e40a734973dc1664bd7578
rschaden <rschaden@gmail.com>
2012-12-07 09:38:17 +0100

    implement date specs

--- spec/core_ext/date_spec.rb
+++ spec/core_ext/date_spec.rb
@@ -1,22 +1,14 @@
 require 'spec_helper'
 
 describe Date do
-  before :each do
-    @date = DateTime.new(2012, 5, 5, 12, 30, 0)
-  end
-
   it '#to_gm_time' do
-    pending 'test only public methods'
-    date_with_new_offset = @date.new_offset
-    @date.should_receive(:convert_to_time).with(date_with_new_offset, :gm)
-    @date.to_gm_time
+    date = DateTime.now
+    date.to_gm_time.should be_within(1).of Time.now.utc
   end
 
   it '#to_local_time' do
-    pending 'test only public methods'
-    date_with_new_offset = @date.new_offset(DateTime.now.offset - @date.send(:offset))
-    @date.should_receive(:convert_to_time).with(date_with_new_offset, :local).once
-    @date.to_local_time
+    date = DateTime.now.new_offset
+    date.to_local_time.should be_within(1).of Time.now
   end
 
 end
