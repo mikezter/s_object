778a7431ac90cd8b59830f8246420737134e0261
Raynon <bennet.palluthe@kaeuferportal.de>
2013-09-23 12:17:35 +0200

    change error to error class name

--- lib/s_object/errors.rb
+++ lib/s_object/errors.rb
@@ -12,13 +12,13 @@ module SObject
     end
   end
 
-  QueryTooComplicatedError = Class.new SalesforceError
-  ObjectNotFoundError      = Class.new SalesforceError
-  DuplicateValueError      = Class.new SalesforceError
-  SessionError             = Class.new SalesforceError
-  ValidationError          = Class.new SalesforceError
-  InvalidFieldError        = Class.new SalesforceError
-  InsertUpdateActivate     = Class.new SalesforceError
+  QueryTooComplicatedError      = Class.new SalesforceError
+  ObjectNotFoundError           = Class.new SalesforceError
+  DuplicateValueError           = Class.new SalesforceError
+  SessionError                  = Class.new SalesforceError
+  ValidationError               = Class.new SalesforceError
+  InvalidFieldError             = Class.new SalesforceError
+  InsertUpdateActivateError     = Class.new SalesforceError
 
   def self.error_class_for(message, code)
     error_class = case code
@@ -28,7 +28,7 @@ module SObject
       when 'INVALID_SESSION_ID'                    then SessionError
       when 'FIELD_CUSTOM_VALIDATION_EXCEPTION'     then ValidationError
       when 'INVALID_FIELD'                         then InvalidFieldError
-      when 'CANNOT_INSERT_UPDATE_ACTIVATE_ENTITY'  then InsertUpdateActivate
+      when 'CANNOT_INSERT_UPDATE_ACTIVATE_ENTITY'  then InsertUpdateActivateError
       else                                         SalesforceError
     end
 
